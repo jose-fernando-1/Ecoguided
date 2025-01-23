12a308fedc8cfb2e7795aa82e67913e5947f4ecc
Caio Gabriel <gabrielnunesdelima2003@gmail.com>
2025-01-23 15:47:48 -0300

    inicio do topbar na landing page

--- Frontend/nextapp/pages/LandingPage/index.tsx
+++ Frontend/nextapp/pages/LandingPage/index.tsx
@@ -1,16 +1,40 @@
 import React from 'react'
 import img_logo from '../../img/img_logo.png'
 import Image from 'next/image'
+import styles from'./index.module.css'
 const LandingPage = () => {
   return (
     <div>
-      <div id='topbar'>
+      <div className={styles['top-bar']}>
         <Image
         src={img_logo}
         alt=''
         width={150}
-        height={50}
+        height={35}
         />
+
+        <a className={styles['link-a']} href='' >Os mais Procurados</a>
+        <a className={styles['link-a']} href='' >Quem Somos</a>
+        <a className={styles['link-a']} >Seja um EcoGuia</a>
+
+        
+          <button 
+            className="" 
+            style={{height:'48px', width:'100px',backgroundColor:'#ffff', color:'#2E747F', borderRadius:'10px',
+              border: '1px solid #ccc',
+              boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.2)', // Sombra 3D inicial
+              transition: 'all 0.2s ease-in-out' // Suavidade na transição
+            }}
+            >Cadastrar</button>
+
+            <button 
+            className="" 
+            style={{height:'48px', width:'100px',backgroundColor:'#2E747F', color:'#ffff', borderRadius:'10px',
+              border: '1px solid #ccc',
+              boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.2)', // Sombra 3D inicial
+              transition: 'all 0.2s ease-in-out' // Suavidade na transição
+            }}
+            >Entrar</button>
       </div>
     </div>
   )
