6f8d3c963bfe7022a61d51b2725040e1481970c1
Caio Gabriel <gabrielnunesdelima2003@gmail.com>
2025-01-28 18:26:41 -0300

    Adicionando tela de login, e primeiros passos dela

---
+++ Frontend/nextapp/pages/Login.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from '../styles/Login.module.css'
+
+const Login = () => {
+  return (
+    <div className={styles['div']}>
+      <div className={styles['div-inside']}></div>
+    </div>
+  )
+}
+
+export default Login
