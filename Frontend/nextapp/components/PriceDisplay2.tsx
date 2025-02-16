0c1329c2d77a2f0653696d280af135ac4ccd7573
Caio Gabriel <gabrielnunesdelima2003@gmail.com>
2025-02-16 14:50:51 -0300

    Terminando tela de carrinho

---
+++ Frontend/nextapp/components/PriceDisplay2.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styles from "../styles/PriceDisplay.module.css";
+
+interface PriceDisplayProps {
+  originalPrice: number;
+  discountedPrice: number;
+}
+
+export default function PriceDisplay2({
+  originalPrice,
+  discountedPrice,
+}: PriceDisplayProps) {
+  return (
+    <div className={styles.container}>
+      <span className={styles.originalPrice}>R${originalPrice}</span>
+      <span className={styles.discountedPrice}>R${discountedPrice}</span>
+    </div>
+  );
+}
