9b9d677ec151ba1039ae6919567d74326976a6a9
jose-fernando-1 <107815035+jose-fernando-1@users.noreply.github.com>
2025-03-07 17:35:10 -0300

    CORS gateway

--- api-gateway/main.py
+++ api-gateway/main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routes import router
 
 app = FastAPI(title="API Gateway")
+
+origins = [
+    "http://127.0.0.1:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_methods=["GET", "POST", "PATCH", "DELETE"],
+    allow_headers=["*"],
+)
 
 app.include_router(router)
 
